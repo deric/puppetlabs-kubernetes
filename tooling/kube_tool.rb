8c5ed07c235121c6dc36826f13b1e7515371816b
Tomas Barton <barton.tomas@gmail.com>
2022-07-20 12:00:58 +0200

    Run directly ruby script, avoid code repetition

--- tooling/kube_tool.rb
+++ tooling/kube_tool.rb
@@ -10,15 +10,15 @@ class Kube_tool
   def self.parse_args
     begin
       opts = Slop.parse do |o|
-        o.string '-o', '--os', 'The OS that Kubernetes will run on', default: ENV['OS'], required: true
-        o.string '-v', '--version', 'The Kubernetes version to install', default: ENV['VERSION'], required: true
-        o.string '-r', '--container_runtime', 'The container runtime to use. This can only be "docker" or "cri_containerd"', default: ENV['CONTAINER_RUNTIME'], required: true
+        o.string '-o', '--os', 'The OS that Kubernetes will run on', default: ENV['OS']
+        o.string '-v', '--version', 'The Kubernetes version to install', default: ENV['VERSION']
+        o.string '-r', '--container_runtime', 'The container runtime to use. This can only be "docker" or "cri_containerd"', default: ENV['CONTAINER_RUNTIME']
         o.string '-c', '--cni_provider', 'The networking provider to use, flannel, weave, calico, calico-tigera or cilium are supported', default: ENV['CNI_PROVIDER']
         o.string '-p', '--cni_provider_version', 'The networking provider version to use, calico and cilium will use this to reference the correct deployment download link', default: ENV['CNI_PROVIDER_VERSION']
         o.string '-t', '--etcd_ip', 'The IP address etcd will listen on', default: ENV['ETCD_IP']
         o.string '-i', '--etcd_initial_cluster', 'The list of servers in the etcd cluster', default: ENV['ETCD_INITIAL_CLUSTER']
         o.string '-a', '--api_address', 'The IP address (or fact) that kube api will listen on', default: ENV['ETCD_IP']
-        o.int '-b', '--key_size', 'Specifies the number of bits in the key to create', default: ENV['KEY_SIZE']
+        o.int '-b', '--key_size', 'Specifies the number of bits in the key to create', default: ENV['KEY_SIZE'].to_i
         o.bool '-d', '--install_dashboard', 'Whether install the kube dashboard', default: ENV['INSTALL_DASHBOARD']
         o.on '-h','--help', 'print the help' do
           puts o
@@ -27,6 +27,8 @@ class Kube_tool
       end
 
       options = opts.to_hash
+      options[:key_size] = 2048 if options[:key_size].nil?
+      puts options
       return options
 
     rescue Slop::Error => e
